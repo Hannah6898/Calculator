f8bf164d30bfdd806349762e854e00e62d187f5b
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-03-28 13:19:17 +0100

    Changed input elements to buttons

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // when buttons are pressed, number shows up on main display
 
-let number = document.querySelectorAll(".number");
-
 function showNumber() {
   let mainDisplay = document.querySelector("#main-display");
-  mainDisplay.innerHTML = `${number.value}`;
+  mainDisplay.innerHTML = `${button.value}`;
 }
 
-number.addEventListener("click", showNumber);
+let button = document.querySelectorAll(".button");
+
+button.addEventListener("click", showNumber);
+console.log(button);
