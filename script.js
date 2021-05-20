c3e888c118ad093707dc3a76624b8b8bc77cece5
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-05-20 19:35:10 +0100

    Updating JavaScript

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 //Assign value to each button
 //When button is pressed, value is shown in the console
+//make the number function work
 
 //Access button value on click
 const button = document.querySelectorAll("button");
 
-for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener("click", function () {
-    console.log(button[i].value);
-  });
-}
+//for (let i = 0; i < button.length; i++) {
+//button[i].addEventListener("click", function () {
+//console.log(button[i].value);
+// });
+//}
+
+button.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+    console.log(button.value);
+  }
+});
 
 //.forEach((button) => console.log(button.value));
